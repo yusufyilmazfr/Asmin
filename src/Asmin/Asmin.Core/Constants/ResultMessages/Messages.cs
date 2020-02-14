1f73cf119ce20db44a8d1dce720036760f544a9b
yusufyilmazfr <yusufyilmazfr@gmail.com>
2020-02-14 17:22:17 +0300

    added result messages

---
+++ src/Asmin/Asmin.Core/Constants/ResultMessages/Messages.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asmin.Core.Constants.ResultMessages
+{
+    public static class Messages
+    {
+        public static readonly string UserAdded = "User successfully added.";
+        public static readonly string UserRemoved = "User successfully removed.";
+        public static readonly string UserUpdated = "User successfully updated.";
+    }
+}
