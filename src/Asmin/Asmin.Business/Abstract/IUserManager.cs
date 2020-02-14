e6235fc451b541f10a82b3f5af7d8b0c8c1d069b
yusufyilmazfr <yusufyilmazfr@gmail.com>
2020-02-14 17:24:25 +0300

    user methods were written

--- src/Asmin/Asmin.Business/Abstract/IUserManager.cs
+++ src/Asmin/Asmin.Business/Abstract/IUserManager.cs
@@ -1,12 +1,23 @@
-﻿using Asmin.Entities.Concrete;
+﻿using Asmin.Core.Utilities.Result;
+using Asmin.Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Asmin.Business.Abstract
 {
     public interface IUserManager
     {
-        List<User> GetList();
+        IDataResult<User> GetById(int id);
+        IDataResult<List<User>> GetList();
+        IResult Add(User user);
+        IResult Update(User user);
+        IResult Remove(User user);
+        Task<IDataResult<User>> GetByIdAsync(int id);
+        Task<IDataResult<List<User>>> GetListAsync();
+        Task<IResult> AddAsync(User user);
+        Task<IResult> UpdateAsync(User user);
+        Task<IResult> RemoveAsync(User user);
     }
 }
--- src/Asmin/Asmin.Business/Concrete/UserManager.cs
+++ src/Asmin/Asmin.Business/Concrete/UserManager.cs
@@ -1,9 +1,12 @@
 ﻿using Asmin.Business.Abstract;
+using Asmin.Core.Constants.ResultMessages;
+using Asmin.Core.Utilities.Result;
 using Asmin.DataAccess.Abstract;
 using Asmin.Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Asmin.Business.Concrete
 {
@@ -16,9 +19,64 @@ namespace Asmin.Business.Concrete
             _userDal = userDal;
         }
 
-        public List<User> GetList()
+        public IResult Add(User user)
         {
-            return _userDal.GetList();
+            _userDal.Add(user);
+            return new SuccessResult(Messages.UserAdded);
+        }
+
+        public async Task<IResult> AddAsync(User user)
+        {
+            await _userDal.AddAsnyc(user);
+            return new SuccessResult(Messages.UserAdded);
+        }
+
+        public IDataResult<User> GetById(int id)
+        {
+            var user = _userDal.GetById(id);
+            return new SuccessDataResult<User>(user);
+        }
+
+        public async Task<IDataResult<User>> GetByIdAsync(int id)
+        {
+            var user = await _userDal.GetByIdAsync(id);
+            return new SuccessDataResult<User>(user);
+        }
+
+        public IDataResult<List<User>> GetList()
+        {
+            var users = _userDal.GetList();
+            return new SuccessDataResult<List<User>>(users);
+        }
+
+        public async Task<IDataResult<List<User>>> GetListAsync()
+        {
+            var users = await _userDal.GetListAsync();
+            return new SuccessDataResult<List<User>>(users);
+        }
+
+        public IResult Remove(User user)
+        {
+            _userDal.Remove(user);
+            return new SuccessResult(Messages.UserRemoved);
+        }
+
+        public async Task<IResult> RemoveAsync(User user)
+        {
+            await _userDal.RemoveAsnyc(user);
+            return new SuccessResult(Messages.UserRemoved);
+        }
+
+        public IResult Update(User user)
+        {
+            _userDal.Update(user);
+            return new SuccessResult(Messages.UserUpdated);
+        }
+
+        public async Task<IResult> UpdateAsync(User user)
+        {
+            await _userDal.UpdateAsnyc(user);
+            return new SuccessResult(Messages.UserUpdated);
         }
     }
 }
