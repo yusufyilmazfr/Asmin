763cdd984e896835acdb881014317e94dff02275
yusufyilmazfr <yusufyilmazfr@gmail.com>
2020-02-15 01:51:07 +0300

    Fluent Validation implemented for entity validation rules

--- src/Asmin/Asmin.Business/DependencyModules/Autofac/AutofacDependencyModule.cs
+++ src/Asmin/Asmin.Business/DependencyModules/Autofac/AutofacDependencyModule.cs
@@ -1,13 +1,16 @@
 ﻿using Asmin.Business.Abstract;
 using Asmin.Business.Concrete;
+using Asmin.Business.ValidationRules.FluentValidation;
 using Asmin.DataAccess.Abstract;
 using Asmin.DataAccess.Concrete.EntityFramework;
+using Asmin.Entities.Concrete;
 using Autofac;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Asmin.Business.DependencyModule.Autofac
+namespace Asmin.Business.DependencyModules.Autofac
 {
     public class AutofacDependencyModule : Module
     {
@@ -22,6 +25,7 @@ namespace Asmin.Business.DependencyModule.Autofac
             #region Business Dependency Registration
 
             builder.RegisterType<UserManager>().As<IUserManager>();
+            builder.RegisterType<UserValidator>().As<IValidator<User>>();
 
             #endregion
         }
---
+++ src/Asmin/Asmin.Business/ValidationRules/FluentValidation/UserValidator.cs
@@ -0,0 +1,26 @@
+﻿using Asmin.Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asmin.Business.ValidationRules.FluentValidation
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(user => user.FirstName)
+                .NotEmpty();
+
+            RuleFor(user => user.LastName)
+                .NotEmpty();
+
+            RuleFor(user => user.Email)
+                .EmailAddress();
+
+            RuleFor(user => user.Password)
+                .NotEmpty();
+        }
+    }
+}
