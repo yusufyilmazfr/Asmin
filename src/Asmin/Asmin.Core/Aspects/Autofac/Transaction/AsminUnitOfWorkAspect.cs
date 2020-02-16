263b2b368937e01017fa07f32245516b28ff435c
yusufyilmazfr <yusufyilmazfr@gmail.com>
2020-02-16 22:19:23 +0300

    unit of work design pattern aspect

---
+++ src/Asmin/Asmin.Core/Aspects/Autofac/Transaction/AsminUnitOfWorkAspect.cs
@@ -0,0 +1,29 @@
+﻿using Asmin.Core.Utilities.Interceptor;
+using Castle.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+
+namespace Asmin.Core.Aspects.Autofac.Transaction
+{
+    public class AsminUnitOfWorkAspect : MethodInterception
+    {
+        public override void Intercept(IInvocation invocation)
+        {
+            using (TransactionScope scope = new TransactionScope())
+            {
+                try
+                {
+                    invocation.Proceed();
+                    scope.Complete();
+                }
+                catch (Exception ex)
+                {
+                    scope.Dispose();
+                    throw ex;
+                }
+            }
+        }
+    }
+}
