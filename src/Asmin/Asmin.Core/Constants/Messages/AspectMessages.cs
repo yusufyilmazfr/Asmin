05d60431624b0dc4a5b76ed031bc78b108e4b353
yusufyilmazfr <yusufyilmazfr@gmail.com>
2020-02-16 22:23:01 +0300

    aspect messages

---
+++ src/Asmin/Asmin.Core/Constants/Messages/AspectMessages.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asmin.Core.Constants.Messages
+{
+    public static class AspectMessages
+    {
+        public static string WrongLoggerType = "Wrong Logger Type.";
+    }
+}
