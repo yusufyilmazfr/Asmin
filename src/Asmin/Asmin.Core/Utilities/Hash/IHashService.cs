4266a1832519f51b434b776fe1e194dd345126ab
yusufyilmazfr <yusufyilmazfr@gmail.com>
2020-02-20 21:48:37 +0300

    hashing service has been written

---
+++ src/Asmin/Asmin.Core/Utilities/Hash/IHashService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asmin.Core.Utilities.Hash
+{
+    public interface IHashService
+    {
+        string CreateHash(string text);
+        bool Compare(string hashedText, string plainText);
+    }
+}
