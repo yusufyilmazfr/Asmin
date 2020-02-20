c0eaab473a42670e13eacb077b768de4a2615e35
yusufyilmazfr <yusufyilmazfr@gmail.com>
2020-02-20 21:55:21 +0300

    login operation done in admin area and other views has been written

--- src/Asmin/Asmin.WebMVC/Areas/Admin/Controllers/HomeController.cs
+++ src/Asmin/Asmin.WebMVC/Areas/Admin/Controllers/HomeController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Asmin.Business.Abstract;
+using Asmin.Core.Entities.Concrete;
 using Asmin.Entities.DTO;
+using Asmin.WebMVC.Constants;
+using Asmin.WebMVC.Services.Session;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Asmin.WebMVC.Areas.Admin.Controllers
@@ -12,34 +15,46 @@ namespace Asmin.WebMVC.Areas.Admin.Controllers
     public class HomeController : Controller
     {
         private IUserManager _userManager;
+        private ISessionService _sessionService;
 
-        public HomeController(IUserManager userManager)
+
+        public HomeController(IUserManager userManager, ISessionService sessionService)
         {
             _userManager = userManager;
+            _sessionService = sessionService;
         }
 
         public IActionResult Index()
         {
+            ViewBag.CurrentUser = _sessionService.GetObject<User>(SessionKey.CURRENT_USER);
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
         public async Task<IActionResult> Login(UserLoginDto user)
         {
             var checkUser = await _userManager.Login(user);
 
             if (!checkUser.IsSuccess)
             {
-                ModelState.AddModelError("", checkUser.Message);
-                return View();
+                ModelState.AddModelError("Error", checkUser.Message);
+                return View(user);
             }
 
             if (checkUser.Data == null)
             {
-                ModelState.AddModelError("", "Invalid email or password.");
-                return View();
+                ModelState.AddModelError("InvalidUser", "Invalid email or password.");
+                return View(user);
             }
 
-            return View();
+            _sessionService.SetObject(SessionKey.CURRENT_USER, checkUser.Data);
+
+            return RedirectToAction("Index");
         }
     }
 }
