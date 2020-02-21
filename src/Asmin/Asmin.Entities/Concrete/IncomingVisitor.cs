7ef18e914ba12068f972c80d255efb99044b7ad0
yusufyilmazfr <yusufyilmazfr@gmail.com>
2020-02-21 22:15:24 +0300

    incoming visitor table

---
+++ src/Asmin/Asmin.Entities/Concrete/IncomingVisitor.cs
@@ -0,0 +1,12 @@
+﻿using Asmin.Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asmin.Entities.Concrete
+{
+    public class IncomingVisitor : BaseEntity
+    {
+        public string IpAddress { get; set; }
+    }
+}
