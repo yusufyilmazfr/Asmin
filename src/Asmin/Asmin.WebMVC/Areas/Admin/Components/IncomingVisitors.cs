910ba4d57d5427db3e1a97c72867d3a3a44f645f
yusufyilmazfr <yusufyilmazfr@gmail.com>
2020-02-21 22:20:56 +0300

    user and incoming visitor count component

---
+++ src/Asmin/Asmin.WebMVC/Areas/Admin/Components/IncomingVisitors.cs
@@ -0,0 +1,26 @@
+﻿using Asmin.Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asmin.WebMVC.Areas.Admin.Components
+{
+    public class IncomingVisitors : ViewComponent
+    {
+        private IIncomingVisitorManager _incomingVisitorManager;
+
+        public IncomingVisitors(IIncomingVisitorManager incomingVisitorManager)
+        {
+            _incomingVisitorManager = incomingVisitorManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var visitorsCountResult = await _incomingVisitorManager.GetCountAsync();
+
+            return View(visitorsCountResult.Data);
+        }
+    }
+}
---
+++ src/Asmin/Asmin.WebMVC/Areas/Admin/Components/RegisteredUsersCount.cs
@@ -0,0 +1,26 @@
+﻿using Asmin.Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asmin.WebMVC.Areas.Admin.Components
+{
+    public class RegisteredUsersCount : ViewComponent
+    {
+        private IUserManager _userManager;
+
+        public RegisteredUsersCount(IUserManager userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var usersCountResul = await _userManager.GetCountAsync();
+
+            return View(usersCountResul.Data);
+        }
+    }
+}
