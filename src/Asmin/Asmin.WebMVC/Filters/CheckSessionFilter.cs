50c7a4e30360032d820dc7227e96c4a5a2729599
yusufyilmazfr <yusufyilmazfr@gmail.com>
2020-02-21 22:21:43 +0300

    check session filter

---
+++ src/Asmin/Asmin.WebMVC/Filters/CheckSessionFilter.cs
@@ -0,0 +1,47 @@
+﻿using Asmin.WebMVC.Constants;
+using Asmin.WebMVC.Services.Session;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asmin.WebMVC.Filters
+{
+    public class CheckSessionFilter : Attribute, IActionFilter
+    {
+        private ISessionService _sessionService;
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            //var any = context
+            //    .ActionDescriptor
+            //    .EndpointMetadata.Select(i => i.GetType().FullName == typeof(SkipCheckSessionFilter).FullName).Any();
+
+            //if (any)
+            //{
+            //    return;
+            //}
+
+            _sessionService = context.HttpContext.RequestServices.GetService<ISessionService>();
+
+            var isUserLoggedIn = _sessionService.Any(SessionKey.CURRENT_USER);
+
+            if (!isUserLoggedIn)
+            {
+                context.Result = new RedirectToRouteResult(new RouteValueDictionary
+                {
+                    {"controller","Home" },{"action","Login"}
+                });
+            }
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+
+        }
+    }
+}
