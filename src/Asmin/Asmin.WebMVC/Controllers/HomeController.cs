26194409404c61d1a332f9b93fd65911d7a8beae
yusufyilmazfr <yusufyilmazfr@gmail.com>
2020-02-25 22:03:28 +0300

    ui design or modifications

--- src/Asmin/Asmin.WebMVC/Controllers/HomeController.cs
+++ src/Asmin/Asmin.WebMVC/Controllers/HomeController.cs
@@ -14,5 +14,10 @@ namespace Asmin.WebMVC.Controllers
         {
             return View();
         }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
     }
 }
