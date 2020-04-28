63f6a71514a9da9aabd047990279095e45eb121e
Yusuf <yusufyilmazfr@gmail.com>
2020-04-28 19:55:29 +0300

    Redis distributed cache service defined.

---
+++ src/Asmin/Asmin.Core/CrossCuttingConcerns/Caching/RedisCache/RedisMemoryCacheService.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Asmin.Core.CrossCuttingConcerns.Caching.RedisCache
+{
+    class RedisMemoryCacheService : ICacheService
+    {
+        private readonly RedisServer _redisServer;
+
+        public RedisMemoryCacheService(RedisServer redisServer)
+        {
+            _redisServer = redisServer;
+        }
+
+        public void Add(string key, object data, int timeout)
+        {
+            var jsonData = JsonConvert.SerializeObject(data);
+            _redisServer.Database.StringSet(key, jsonData, TimeSpan.FromMinutes(timeout));
+        }
+
+        public bool Any(string key)
+        {
+            return _redisServer.Database.KeyExists(key);
+        }
+
+        public void Clear()
+        {
+            _redisServer.ClearAllKey();
+        }
+
+        public T Get<T>(string key)
+        {
+            if (Any(key))
+            {
+                string jsonData = _redisServer.Database.StringGet(key);
+                return JsonConvert.DeserializeObject<T>(jsonData);
+            }
+
+            return default(T);
+        }
+
+        public object Get(string key)
+        {
+            return Get<object>(key);
+        }
+
+        public void RemoveByKey(string key)
+        {
+            if (Any(key))
+            {
+                _redisServer.Database.KeyDelete(key);
+            }
+        }
+
+        public void RemoveByPattern(string pattern)
+        {
+            var regex = new Regex(pattern, RegexOptions.Singleline | RegexOptions.IgnoreCase);
+
+            foreach (string key in _redisServer.GetAllKeys())
+            {
+                if (regex.IsMatch(key))
+                {
+                    RemoveByKey(key);
+                }
+            }
+        }
+    }
+}
---
+++ src/Asmin/Asmin.Core/DependencyModules/RedisCacheModule.cs
@@ -0,0 +1,18 @@
+﻿using Asmin.Core.CrossCuttingConcerns.Caching;
+using Asmin.Core.CrossCuttingConcerns.Caching.RedisCache;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asmin.Core.DependencyModules
+{
+    public class RedisCacheModule : ICoreModule
+    {
+        public void Load(IServiceCollection services)
+        {
+            services.AddSingleton<RedisServer>();
+            services.AddSingleton<ICacheService, RedisMemoryCacheService>();
+        }
+    }
+}
