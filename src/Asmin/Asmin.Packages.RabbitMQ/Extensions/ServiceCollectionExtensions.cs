01799ac14e05d102ac20ad2d74636e2fca3986a9
Yusuf <yusufyilmazfr@gmail.com>
2021-01-01 01:00:49 +0300

    IServiceCollection extension method was defined for package dependencies

---
+++ src/Asmin/Asmin.Packages.RabbitMQ/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Asmin.Packages.RabbitMQ.Configuration;
+using Asmin.Packages.RabbitMQ.Publisher;
+using Asmin.Packages.RabbitMQ.Service;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Asmin.Packages.RabbitMQ.Extensions
+{
+    /// <summary>
+    /// ServiceCollectionExtensions contains extended IServiceCollection's methods.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Extension method for IServiceCollection
+        /// </summary>
+        /// <returns></returns>
+        public static IServiceCollection AddRabbitMQ(this IServiceCollection services, RabbitMQConfiguration configuration)
+        {
+            services.AddSingleton(typeof(IRabbitMQConfiguration), configuration);
+
+            services.AddSingleton<IRabbitMQService, RabbitMQService>();
+
+            services.AddSingleton<IRabbitMQPublisherService, RabbitMQPublisherService>();
+
+            return services;
+        }
+    }
+}
