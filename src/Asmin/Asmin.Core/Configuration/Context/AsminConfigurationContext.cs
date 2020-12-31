d085bb53814599cc4242b66b77a75c8d62644daa
Yusuf <yusufyilmazfr@gmail.com>
2021-01-01 01:06:07 +0300

    I wrote RabbitMQ properties in AsminConfigurationContext

--- src/Asmin/Asmin.Core/Configuration/Context/AsminConfigurationContext.cs
+++ src/Asmin/Asmin.Core/Configuration/Context/AsminConfigurationContext.cs
@@ -12,13 +12,23 @@ namespace Asmin.Core.Configuration.Context
         public string RedisUsername { get; set; }
         public string RedisPassword { get; set; }
 
+
+        public string RabbitMQHost { get; set; }
+        public int RabbitMQPort { get; set; }
+        public string RabbitMQUsername { get; set; }
+        public string RabbitMQPassword { get; set; }
+
         public AsminConfigurationContext(IEnvironmentService environmentService)
         {
             RedisHost = environmentService.Configuration["Redis:Host"];
             RedisPort = int.Parse(environmentService.Configuration["Redis:Port"]);
             RedisUsername = environmentService.Configuration["Redis:Username"];
             RedisPassword = environmentService.Configuration["Redis:Password"];
-        }
 
+            RabbitMQHost = environmentService.Configuration["RabbitMQ:Host"];
+            RabbitMQPort = int.Parse(environmentService.Configuration["RabbitMQ:Port"]);
+            RabbitMQUsername = environmentService.Configuration["RabbitMQ:Username"];
+            RabbitMQPassword = environmentService.Configuration["RabbitMQ:Password"];
+        }
     }
 }
