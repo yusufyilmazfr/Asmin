5f7e895f0214b5655c07cff509a6daf66185a84a
Yusuf <yusufyilmazfr@gmail.com>
2020-12-31 15:32:31 +0300

    configuration context was added

---
+++ src/Asmin/Asmin.Core/Configuration/Context/AsminConfigurationContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Asmin.Core.Configuration.Environment;
+
+namespace Asmin.Core.Configuration.Context
+{
+    public class AsminConfigurationContext : IAsminConfigurationContext
+    {
+        public string RedisHost { get; set; }
+        public int RedisPort { get; set; }
+        public string RedisUsername { get; set; }
+        public string RedisPassword { get; set; }
+
+        public AsminConfigurationContext(IEnvironmentService environmentService)
+        {
+            RedisHost = environmentService.Configuration["Redis:Host"];
+            RedisPort = int.Parse(environmentService.Configuration["Redis:Port"]);
+            RedisUsername = environmentService.Configuration["Redis:Username"];
+            RedisPassword = environmentService.Configuration["Redis:Password"];
+        }
+
+    }
+}
---
+++ src/Asmin/Asmin.Core/Configuration/Context/IAsminConfigurationContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asmin.Core.Configuration.Context
+{
+    /// <summary>
+    /// This interface provides value of configuration properties.
+    /// </summary>
+    public interface IAsminConfigurationContext
+    {
+        string RedisHost { get; set; }
+        int RedisPort { get; set; }
+        string RedisUsername { get; set; }
+        string RedisPassword { get; set; }
+    }
+}
