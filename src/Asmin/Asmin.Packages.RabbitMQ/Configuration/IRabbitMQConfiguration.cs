63113dc786531d12016f3b5890dc07f89437403c
Yusuf <yusufyilmazfr@gmail.com>
2021-01-01 00:56:58 +0300

    RabbitMQ configuration services were defined

---
+++ src/Asmin/Asmin.Packages.RabbitMQ/Configuration/IRabbitMQConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asmin.Packages.RabbitMQ.Configuration
+{
+    /// <summary>
+    /// RabbitMQ configuration
+    /// </summary>
+    public interface IRabbitMQConfiguration
+    {
+        /// <summary>
+        /// RabbitMQ host name
+        /// </summary>
+        public string Host { get; set; }
+        /// <summary>
+        /// RabbitMQ port
+        /// </summary>
+        public int Port { get; set; }
+        /// <summary>
+        /// RabbitMQ username
+        /// </summary>
+        public string Username { get; set; }
+        /// <summary>
+        /// RabbitMQ password
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
