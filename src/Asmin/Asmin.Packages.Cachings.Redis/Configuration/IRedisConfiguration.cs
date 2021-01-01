21d5eba0fa862059d5a9f11a62bf4bc1097d0726
Yusuf <yusufyilmazfr@gmail.com>
2021-01-01 20:09:12 +0300

    Redis configuration class and interface was defined

---
+++ src/Asmin/Asmin.Packages.Cachings.Redis/Configuration/IRedisConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asmin.Packages.Cachings.Redis.Configuration
+{
+    /// <summary>
+    /// Redis server configuration 
+    /// </summary>
+    public interface IRedisConfiguration
+    {
+        /// <summary>
+        /// Redis server hostname
+        /// </summary>
+        string Host { get; set; }
+        /// <summary>
+        /// Redis server password
+        /// </summary>
+        string Password { get; set; }
+        /// <summary>
+        /// Redis server port
+        /// </summary>
+        int Port { get; set; }
+    }
+}
---
+++ src/Asmin/Asmin.Packages.Cachings.Redis/Configuration/RedisConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asmin.Packages.Cachings.Redis.Configuration
+{
+    /// <summary>
+    /// Redis server configuration 
+    /// </summary>
+    public class RedisConfiguration : IRedisConfiguration
+    {
+        /// <summary>
+        /// Redis server hostname
+        /// </summary>
+        public string Host { get; set; }
+        /// <summary>
+        /// Redis server password
+        /// </summary>
+        public string Password { get; set; }
+        /// <summary>
+        /// Redis server port
+        /// </summary>
+        public int Port { get; set; }
+    }
+}
