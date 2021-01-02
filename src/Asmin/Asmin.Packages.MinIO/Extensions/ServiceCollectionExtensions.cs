763909021cd697aee82b27bc0665504a8847fa78
Yusuf <yusufyilmazfr@gmail.com>
2021-01-02 15:31:32 +0300

    I wrote IServiceCollection extension method and registered package dependencies.

---
+++ src/Asmin/Asmin.Packages.MinIO/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Asmin.Packages.MinIO.Client;
+using Asmin.Packages.MinIO.Configuration;
+using Asmin.Packages.MinIO.Service;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Asmin.Packages.MinIO.Extensions
+{
+    /// <summary>
+    /// ServiceCollectionExtensions contains extended IServiceCollection's methods.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Register Asmin.Packages.MinIO package dependencies.
+        /// </summary>
+        /// <returns></returns>
+        public static IServiceCollection AddMinIO(this IServiceCollection services, MinIOConfiguration configuration)
+        {
+            services.AddSingleton(typeof(IMinIOConfiguration), configuration);
+
+            services.AddSingleton<IMinIOClientFactory, MinIOClientFactory>();
+            services.AddSingleton<IMinIOService, MinIOService>();
+
+            return services;
+        }
+    }
+}
