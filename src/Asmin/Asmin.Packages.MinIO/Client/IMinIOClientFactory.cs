9281d6f688a4614bb8671d6d2c9b4f47304d81bc
Yusuf <yusufyilmazfr@gmail.com>
2021-01-02 15:29:02 +0300

    Defined MinIO client factory

---
+++ src/Asmin/Asmin.Packages.MinIO/Client/IMinIOClientFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Minio;
+
+namespace Asmin.Packages.MinIO.Client
+{
+    /// <summary>
+    /// IMinIOClient provides MinIO client instance.
+    /// </summary>
+    interface IMinIOClientFactory
+    {
+        /// <summary>
+        /// Current instance
+        /// </summary>
+        MinioClient Client { get; }
+    }
+}
---
+++ src/Asmin/Asmin.Packages.MinIO/Client/MinIOClientFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Asmin.Packages.MinIO.Configuration;
+using Minio;
+
+namespace Asmin.Packages.MinIO.Client
+{
+    class MinIOClientFactory : IMinIOClientFactory
+    {
+        public MinioClient Client { get; }
+
+        public MinIOClientFactory(IMinIOConfiguration configuration)
+        {
+            Client = new MinioClient(configuration.EndPoint, configuration.AccessKey, configuration.SecretKey);
+        }
+    }
+}
