481b425c6a070e48a55dbcc3a8a84cc5991483b2
Yusuf <yusufyilmazfr@gmail.com>
2021-01-02 15:30:34 +0300

    Some MinIO operations defined in services such as bucket, object, presigned

---
+++ src/Asmin/Asmin.Packages.MinIO/Service/IMinIOService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Minio.DataModel;
+
+namespace Asmin.Packages.MinIO.Service
+{
+    /// <summary>
+    /// This interface provides MinIO client methods.
+    /// </summary>
+    public interface IMinIOService
+    {
+        /// <summary>
+        /// Make bucket to MinIO server.
+        /// It creates new bucket when bucket does not exists.
+        /// Return true when bucket was created successfully or bucket exists.
+        /// </summary>
+        /// <param name="bucketName">MinIO bucket name</param>
+        /// <param name="loc">Location, not required. Default us-east-1</param>
+        /// <returns></returns>
+        Task<bool> MakeBucketAsync(string bucketName, string loc = "us-east-1");
+        /// <summary>
+        /// Remove specified bucket. Return false when operation unsuccessful or bucket does not exists
+        /// </summary>
+        /// <param name="bucketName">Bucket name</param>
+        /// <returns></returns>
+        Task<bool> RemoveBucketAsync(string bucketName);
+        /// <summary>
+        /// Check specified bucket exists.
+        /// </summary>
+        /// <param name="bucketName">Bucket name</param>
+        /// <returns></returns>
+        Task<bool> BucketExistsAsync(string bucketName);
+        /// <summary>
+        /// Create an async task for listing buckets.
+        /// </summary>
+        /// <returns></returns>
+        Task<ListAllMyBucketsResult> GetBucketListAsync();
+        /// <summary>
+        /// Add file to MinIO server. If the bucket doesn't exist we create new bucket with specified bucket name.
+        /// </summary>
+        /// <param name="stream">File stream</param>
+        /// <param name="bucketName">MinIO bucket name</param>
+        /// <param name="fileName">MinIO file name</param>
+        /// <returns></returns>
+        Task<bool> UploadFileAsync(MemoryStream stream, string bucketName, string fileName);
+        /// <summary>
+        /// Remove file from MinIO server.
+        /// </summary>
+        /// <param name="bucketName">MinIO bucket name</param>
+        /// <param name="fileName">MinIO file name</param>
+        /// <returns></returns>
+        Task<bool> DeleteFileAsync(string bucketName, string fileName);
+        /// <summary>
+        /// Check file exists. Return true when file exists otherwise false.
+        /// </summary>
+        /// <param name="bucketName"></param>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        Task<bool> CheckFileExistsAsync(string bucketName, string fileName);
+        /// <summary>
+        /// Get presigned object link from MinIO server. Return empty string when file or bucket does not exist.
+        /// </summary>
+        /// <param name="bucketName">MinIO bucket name</param>
+        /// <param name="fileName">MinIO file name</param>
+        /// <param name="expirySecond">Expiry second, default 1 day.</param>
+        /// <returns></returns>
+        Task<string> GetPresignedObjectUrlAsync(string bucketName, string fileName, int expirySecond = 86400);
+    }
+}
---
+++ src/Asmin/Asmin.Packages.MinIO/Service/MinIOService.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Asmin.Packages.MinIO.Client;
+using Minio;
+using Minio.DataModel;
+
+namespace Asmin.Packages.MinIO.Service
+{
+    class MinIOService : IMinIOService
+    {
+        private MinioClient _minioClient;
+
+        public MinIOService(IMinIOClientFactory clientFactory)
+        {
+            _minioClient = clientFactory.Client;
+        }
+
+        public async Task<bool> MakeBucketAsync(string bucketName, string loc = "us-east-1")
+        {
+            bool returnValue = false;
+
+            try
+            {
+                bool checkBucketExists = await _minioClient.BucketExistsAsync(bucketName);
+
+                if (!checkBucketExists)
+                {
+                    await _minioClient.MakeBucketAsync(bucketName, loc);
+                }
+
+                returnValue = true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"[Make-Bucket]  Exception: {e}");
+            }
+
+            return returnValue;
+        }
+
+        public async Task<bool> RemoveBucketAsync(string bucketName)
+        {
+            bool returnValue = false;
+
+            try
+            {
+                bool checkBucketExists = await _minioClient.BucketExistsAsync(bucketName);
+
+                if (checkBucketExists)
+                {
+                    await _minioClient.RemoveBucketAsync(bucketName);
+                    returnValue = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"[Remove-Bucket]  Exception: {e}");
+            }
+
+            return returnValue;
+        }
+
+        public Task<bool> BucketExistsAsync(string bucketName)
+        {
+            return _minioClient.BucketExistsAsync(bucketName);
+        }
+
+        public Task<ListAllMyBucketsResult> GetBucketListAsync()
+        {
+            return _minioClient.ListBucketsAsync();
+        }
+
+        public async Task<bool> UploadFileAsync(MemoryStream stream, string bucketName, string fileName)
+        {
+            bool returnValue = false;
+
+            try
+            {
+                bool checkBucketExists = await _minioClient.BucketExistsAsync(bucketName);
+
+                if (!checkBucketExists)
+                {
+                    await _minioClient.MakeBucketAsync(bucketName);
+                }
+
+                await _minioClient.PutObjectAsync(bucketName, fileName, stream, stream.Length);
+
+                returnValue = true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"[Upload-Object]  Exception: {e}");
+            }
+
+            return returnValue;
+        }
+
+        public async Task<bool> DeleteFileAsync(string bucketName, string fileName)
+        {
+            bool returnValue = false;
+
+            try
+            {
+                await _minioClient.RemoveObjectAsync(bucketName, fileName);
+                returnValue = true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"[Delete-Object]  Exception: {e}");
+            }
+
+            return returnValue;
+        }
+
+        public async Task<bool> CheckFileExistsAsync(string bucketName, string fileName)
+        {
+            bool returnValue = false;
+
+            try
+            {
+                var objectStat = await _minioClient.StatObjectAsync(bucketName, fileName);
+
+                returnValue = objectStat != null;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"[Object-Status] Exception: {e.Message}");
+            }
+
+            return returnValue;
+        }
+
+        public Task<string> GetPresignedObjectUrlAsync(string bucketName, string fileName, int expirySecond = 86400)
+        {
+            try
+            {
+                return _minioClient.PresignedGetObjectAsync(bucketName, fileName, expirySecond);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"[Presigned-Object-Url] Exception: {e.Message}");
+                return Task.FromResult<string>("");
+            }
+        }
+    }
+}
