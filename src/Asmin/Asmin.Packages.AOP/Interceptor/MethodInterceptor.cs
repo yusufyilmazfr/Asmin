9cd5d7aed3083ecf71162959d9683174aba3f9f3
Yusuf <yusufyilmazfr@gmail.com>
2021-01-05 22:01:37 +0300

    Defined summary to methods

--- src/Asmin/Asmin.Packages.AOP/Interceptor/MethodInterceptor.cs
+++ src/Asmin/Asmin.Packages.AOP/Interceptor/MethodInterceptor.cs
@@ -5,13 +5,40 @@ using Castle.DynamicProxy;
 
 namespace Asmin.Packages.AOP.Interceptor
 {
+    /// <summary>
+    /// MethodInterceptor provides writing aspect.
+    /// </summary>
     public class MethodInterceptor : MethodInterceptorBase
     {
+        /// <summary>
+        /// OnBefore works before method working
+        /// </summary>
+        /// <param name="invocation"></param>
         public virtual void OnBefore(IInvocation invocation) { }
+
+        /// <summary>
+        /// OnAfter works last
+        /// </summary>
+        /// <param name="invocation"></param>
         public virtual void OnAfter(IInvocation invocation) { }
+
+        /// <summary>
+        /// OnSuccess works when method done successfully
+        /// </summary>
+        /// <param name="invocation"></param>
         public virtual void OnSuccess(IInvocation invocation) { }
+
+        /// <summary>
+        /// OnException works when method throws exception
+        /// </summary>
+        /// <param name="invocation"></param>
+        /// <param name="exception">Throwed exception</param>
         public virtual void OnException(IInvocation invocation, Exception exception) { }
 
+        /// <summary>
+        /// Intercept works while method is working
+        /// </summary>
+        /// <param name="invocation"></param>
         public override void Intercept(IInvocation invocation)
         {
             bool isSuccess = true;
