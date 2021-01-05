4343ab3c930bd998be468d499603b5ee8e8378fb
Yusuf <yusufyilmazfr@gmail.com>
2021-01-05 21:56:30 +0300

    Created interceptor classses

---
+++ src/Asmin/Asmin.Packages.AOP/Interceptor/MethodInterceptor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Castle.DynamicProxy;
+
+namespace Asmin.Packages.AOP.Interceptor
+{
+    public class MethodInterceptor : MethodInterceptorBase
+    {
+        public virtual void OnBefore(IInvocation invocation) { }
+        public virtual void OnAfter(IInvocation invocation) { }
+        public virtual void OnSuccess(IInvocation invocation) { }
+        public virtual void OnException(IInvocation invocation, Exception exception) { }
+
+        public override void Intercept(IInvocation invocation)
+        {
+            bool isSuccess = true;
+
+            OnBefore(invocation);
+
+            try
+            {
+                invocation.Proceed();
+            }
+            catch (Exception e)
+            {
+                isSuccess = false;
+
+                OnException(invocation, e);
+            }
+            finally
+            {
+                if (isSuccess)
+                {
+                    OnSuccess(invocation);
+                }
+
+                OnAfter(invocation);
+            }
+        }
+    }
+}
