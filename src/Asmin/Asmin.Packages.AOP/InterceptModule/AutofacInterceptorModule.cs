fcab11073538c3f7c70efe370149881ae808a363
Yusuf <yusufyilmazfr@gmail.com>
2021-01-05 22:14:39 +0300

    I wrote Intercept module in AOP package

---
+++ src/Asmin/Asmin.Packages.AOP/InterceptModule/AutofacInterceptorModule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Asmin.Packages.AOP.Interceptor;
+using Autofac;
+using Autofac.Extras.DynamicProxy;
+using Castle.DynamicProxy;
+
+namespace Asmin.Packages.AOP.InterceptModule
+{
+    public class AutofacInterceptorModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            var executingAssembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+            builder.RegisterAssemblyTypes(executingAssembly)
+                .AsImplementedInterfaces()
+                .EnableInterfaceInterceptors(new ProxyGenerationOptions()
+                {
+                    Selector = new AspectInterceptorSelector()
+                })
+                .SingleInstance();
+        }
+    }
+}
