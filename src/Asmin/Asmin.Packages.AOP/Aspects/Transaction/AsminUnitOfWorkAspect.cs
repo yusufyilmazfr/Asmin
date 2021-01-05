aabfa6c9d77605e9f9cc8b9ac614de396689d143
Yusuf <yusufyilmazfr@gmail.com>
2021-01-05 22:03:42 +0300

    Defined unit of work aspect

---
+++ src/Asmin/Asmin.Packages.AOP/Aspects/Transaction/AsminUnitOfWorkAspect.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+using Asmin.Packages.AOP.Interceptor;
+using Castle.DynamicProxy;
+
+namespace Asmin.Packages.AOP.Aspects.Transaction
+{
+    /// <summary>
+    /// AsminUnitOfWorkAspect works when method is work unsuccessfully. Throws expected exception after transaction.
+    /// </summary>
+    public class AsminUnitOfWorkAspect : MethodInterceptor
+    {
+        public override void Intercept(IInvocation invocation)
+        {
+            using (TransactionScope transactionScope = new TransactionScope())
+            {
+                try
+                {
+                    invocation.Proceed();
+                    transactionScope.Complete();
+                }
+                catch (System.Exception e)
+                {
+                    transactionScope.Dispose();
+                    throw;
+                }
+            }
+        }
+    }
+}
