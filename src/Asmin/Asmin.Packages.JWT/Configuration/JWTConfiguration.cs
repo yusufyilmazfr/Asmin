d4be5a00358c73782cdce62c5fe6351315a759d0
Yusuf <yusufyilmazfr@gmail.com>
2021-01-09 00:58:54 +0300

    I created JWT configuration class

---
+++ src/Asmin/Asmin.Packages.JWT/Configuration/JWTConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Asmin.Packages.JWT.Entities;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Asmin.Packages.JWT.Configuration
+{
+    public class JWTConfiguration : IJWTConfiguration
+    {
+        public string SecretKey { get; set; }
+        public int ExpiryHour { get; set; }
+        public EnumTokenSecurityAlgorithms TokenSecurityAlgorithms { get; set; }
+    }
+}
