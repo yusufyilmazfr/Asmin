1e22c8f15789ae88c3fbef5c57261ec196ebda98
Yusuf <yusufyilmazfr@gmail.com>
2021-01-09 00:59:56 +0300

    Created TokenResult class, it contains token and expiry date.

---
+++ src/Asmin/Asmin.Packages.JWT/Result/TokenResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asmin.Packages.JWT.Result
+{
+    /// <summary>
+    /// Token result contains JWT token result.
+    /// </summary>
+    public class TokenResult
+    {
+        /// <summary>
+        /// Generated token.
+        /// </summary>
+        public string Token { get; set; }
+        /// <summary>
+        /// Expiry date of token.
+        /// </summary>
+        public DateTime ExpiryDate { get; set; }
+    }
+}
