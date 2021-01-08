59f308112ec3c03b3b0763c531bb1ddd6fec359e
Yusuf <yusufyilmazfr@gmail.com>
2021-01-08 18:48:29 +0300

    I have added some summary to Result structures.

--- src/Asmin/Asmin.Core/Utilities/Result/IResult.cs
+++ src/Asmin/Asmin.Core/Utilities/Result/IResult.cs
@@ -4,9 +4,18 @@ using System.Text;
 
 namespace Asmin.Core.Utilities.Result
 {
+    /// <summary>
+    /// IResult provides base information for method status.
+    /// </summary>
     public interface IResult
     {
+        /// <summary>
+        /// Check method done successfully.
+        /// </summary>
         bool IsSuccess { get; }
+        /// <summary>
+        /// Result message.
+        /// </summary>
         string Message { get; }
     }
 }
--- src/Asmin/Asmin.Core/Utilities/Result/SuccessDataResult.cs
+++ src/Asmin/Asmin.Core/Utilities/Result/SuccessDataResult.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace Asmin.Core.Utilities.Result
 {
+    /// <summary>
+    /// Success data result for successfully done method.
+    /// </summary>
     public class SuccessDataResult<T> : DataResult<T>
     {
         public SuccessDataResult(T data) : base(data, true)
