b852026d502abb86b08dd6378200e89afd6a59ee
Yusuf <yusufyilmazfr@gmail.com>
2021-01-08 21:42:10 +0300

    Added hash core package. This package contains hashing methods signatures.

---
+++ src/Asmin/Asmin.Packages.Hashing.Core/Service/IHashService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asmin.Packages.Hashing.Core.Service
+{
+    /// <summary>
+    /// Hash service provides generate and compare methods signatures.
+    /// </summary>
+    public interface IHashService
+    {
+        /// <summary>
+        /// Generate hash.
+        /// </summary>
+        /// <param name="plainText">Plain text for hashing.</param>
+        /// <returns></returns>
+        string Generate(string plainText);
+        /// <summary>
+        /// Compare hashed text equals plain text.
+        /// </summary>
+        /// <param name="hashedText">Hashed text.</param>
+        /// <param name="plainText">Plain text.</param>
+        /// <returns></returns>
+        bool Compare(string hashedText, string plainText);
+    }
+}
