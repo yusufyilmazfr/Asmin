c8950404509f5feaa7c71765fb3f1c45b6e5f331
Yusuf <yusufyilmazfr@gmail.com>
2021-01-12 15:11:01 +0300

    I added token filter attribute and ignore token attribute 🥳🎉

---
+++ src/Asmin/Asmin.Packages.JWT/Filters/AsminIgnoreTokenAuthFilter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Asmin.Packages.JWT.Filters
+{
+    /// <summary>
+    /// It provides to ignore token control.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class AsminIgnoreTokenAuthFilter : ActionFilterAttribute
+    {
+    }
+}
