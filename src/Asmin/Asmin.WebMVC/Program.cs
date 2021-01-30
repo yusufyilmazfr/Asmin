e096e641ba4066b19d6cea09744720ec19463bcf
Yusuf Yılmaz <yusufyilmazfr@gmail.com>
2021-01-30 14:37:52 +0300

    Removed Business, Autofac and other package dependencies 🎉

--- src/Asmin/Asmin.WebMVC/Program.cs
+++ src/Asmin/Asmin.WebMVC/Program.cs
@@ -1,12 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Asmin.WebMVC
 {
@@ -19,7 +12,6 @@ namespace Asmin.WebMVC
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
