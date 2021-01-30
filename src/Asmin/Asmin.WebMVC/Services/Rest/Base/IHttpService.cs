1872e3d54df33f0083e6d4205dbb49cb9ae3f2d3
Yusuf Yılmaz <yusufyilmazfr@gmail.com>
2021-01-30 23:10:37 +0300

    I created new base service for common http requests.

---
+++ src/Asmin/Asmin.WebMVC/Services/Rest/Base/IHttpService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asmin.WebMVC.Services.Rest.Base
+{
+    public interface IHttpService
+    {
+        Task<T> GetAsync<T>(string url);
+        Task<T> PostAsync<T>(string url, object data);
+    }
+}
