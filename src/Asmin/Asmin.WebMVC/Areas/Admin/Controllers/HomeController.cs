27c534ae86fa04c735767e004b2fb9e9202bec5d
Yusuf Yılmaz <yusufyilmazfr@gmail.com>
2021-01-30 14:35:32 +0300

    I wrote UserApiService. Project communicates with api. I removed old business and other layer dependencies.

--- src/Asmin/Asmin.WebMVC/Areas/Admin/Controllers/HomeController.cs
+++ src/Asmin/Asmin.WebMVC/Areas/Admin/Controllers/HomeController.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Asmin.Business.Abstract;
+﻿using System.Threading.Tasks;
 using Asmin.Core.Entities.Concrete;
 using Asmin.Entities.CustomEntities.Request.User;
 using Asmin.WebMVC.Constants;
 using Asmin.WebMVC.Filters;
+using Asmin.WebMVC.Services.Rest.UserService;
 using Asmin.WebMVC.Services.Session;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Asmin.WebMVC.Areas.Admin.Controllers
@@ -16,14 +14,17 @@ namespace Asmin.WebMVC.Areas.Admin.Controllers
     [TypeFilter(typeof(CheckSessionFilter))]
     public class HomeController : Controller
     {
-        private readonly IUserManager _userManager;
         private readonly ISessionService _sessionService;
+        private readonly IUserApiService _userApiService;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
         public User CurrentUser => _sessionService.Get<User>(SessionKey.CurrentUser);
 
-        public HomeController(IUserManager userManager, ISessionService sessionService)
+        public HomeController(ISessionService sessionService, IUserApiService userApiService, IHttpContextAccessor httpContextAccessor)
         {
-            _userManager = userManager;
             _sessionService = sessionService;
+            _userApiService = userApiService;
+            _httpContextAccessor = httpContextAccessor;
         }
 
         public IActionResult Index()
@@ -44,21 +45,38 @@ namespace Asmin.WebMVC.Areas.Admin.Controllers
 
         [HttpPost]
         [AllowAnySessionFilter]
-        public IActionResult Login(UserLoginRequest user)
+        public async Task<IActionResult> Login(UserLoginRequest userLoginRequest)
         {
-            var checkUser = _userManager.Login(user);
+            var checkUser = await _userApiService.LoginAsync(userLoginRequest);
 
             if (!checkUser.IsSuccess)
             {
                 ModelState.AddModelError("Error", checkUser.Message);
-                return View(user);
+                return View(userLoginRequest);
             }
 
             if (checkUser.Data == null)
             {
                 ModelState.AddModelError("InvalidUser", "Invalid email or password.");
-                return View(user);
+                return View(userLoginRequest);
             }
+
+            var cookieOptions = new CookieOptions
+            {
+                // Set the secure flag, which Chrome's changes will require for SameSite none.
+                // Note this will also require you to be running on HTTPS
+                Secure = true,
+
+                // Set the cookie to HTTP only which is good practice unless you really do need
+                // to access it client side in scripts.
+                HttpOnly = true,
+
+                // Add the SameSite attribute, this will emit the attribute with a value of none.
+                // To not emit the attribute at all set the SameSite property to SameSiteMode.Unspecified.
+                SameSite = SameSiteMode.None
+            };
+
+            _httpContextAccessor.HttpContext.Response.Cookies.Append("token", checkUser.Data.TokenInformation.Token);
 
             _sessionService.Set(SessionKey.CurrentUser, checkUser.Data);
 
@@ -67,6 +85,7 @@ namespace Asmin.WebMVC.Areas.Admin.Controllers
 
         public IActionResult Logout()
         {
+            _httpContextAccessor.HttpContext.Response.Cookies.Delete("token");
             _sessionService.Remove(SessionKey.CurrentUser);
 
             return RedirectToAction("Login");
