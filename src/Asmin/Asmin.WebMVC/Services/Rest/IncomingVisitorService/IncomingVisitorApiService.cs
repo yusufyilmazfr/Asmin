bbcbe691882a47b7610319f49fcff5eecc981bf2
Yusuf Yılmaz <yusufyilmazfr@gmail.com>
2021-01-30 23:12:25 +0300

    refactoring: removed unnecessary request code lines in services. I Used HttpService for common requests.

--- src/Asmin/Asmin.WebMVC/Services/Rest/IncomingVisitorService/IncomingVisitorApiService.cs
+++ src/Asmin/Asmin.WebMVC/Services/Rest/IncomingVisitorService/IncomingVisitorApiService.cs
@@ -1,68 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Threading.Tasks;
-using Asmin.Core.Configuration.Context;
-using Asmin.Core.Entities.Concrete;
+﻿using System.Threading.Tasks;
 using Asmin.Core.Utilities.Result;
 using Asmin.Entities.Concrete;
-using Asmin.Entities.CustomEntities.Response.User;
-using Microsoft.AspNetCore.Http;
-using Newtonsoft.Json;
+using Asmin.WebMVC.Services.Rest.Base;
 
 namespace Asmin.WebMVC.Services.Rest.IncomingVisitorService
 {
     public class IncomingVisitorApiService : IIncomingVisitorApiService
     {
-        private readonly IHttpClientFactory _httpClientFactory;
-        private readonly IHttpContextAccessor _httpContextAccessor;
-        private readonly IAsminConfigurationContext _asminConfigurationContext;
+        private readonly IHttpService _httpService;
 
-        public IncomingVisitorApiService(IHttpClientFactory httpClientFactory, IHttpContextAccessor httpContextAccessor, IAsminConfigurationContext asminConfigurationContext)
+        public IncomingVisitorApiService(IHttpService httpService)
         {
-            _httpClientFactory = httpClientFactory;
-            _httpContextAccessor = httpContextAccessor;
-            _asminConfigurationContext = asminConfigurationContext;
+            _httpService = httpService;
         }
 
-
-        public async Task<Result> AddAsync(IncomingVisitor incomingVisitor)
+        public Task<Result> AddAsync(IncomingVisitor incomingVisitor)
         {
-            var httpClient = _httpClientFactory.CreateClient();
-
-            _httpContextAccessor.HttpContext.Request.Cookies.TryGetValue("token", out string token);
-
-            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-
-            httpClient.BaseAddress = new Uri(_asminConfigurationContext.ApiUrl);
-
-            var httpContent = new StringContent(JsonConvert.SerializeObject(incomingVisitor), Encoding.UTF8, "application/json");
-
-            var responseMessage = await httpClient.PostAsync("/incomingvisitors", httpContent);
-
-            var content = await responseMessage.Content.ReadAsStringAsync();
-
-            return JsonConvert.DeserializeObject<Result>(content);
+            return _httpService.PostAsync<Result>("/incomingvisitors", incomingVisitor);
         }
 
-        public async Task<DataResult<int>> GetCountAsync()
+        public Task<DataResult<int>> GetCountAsync()
         {
-            var httpClient = _httpClientFactory.CreateClient();
-
-            _httpContextAccessor.HttpContext.Request.Cookies.TryGetValue("token", out string token);
-
-            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-
-            httpClient.BaseAddress = new Uri(_asminConfigurationContext.ApiUrl);
-
-            var responseMessage = await httpClient.GetAsync("/incomingvisitors/count");
-
-            var content = await responseMessage.Content.ReadAsStringAsync();
-
-            return JsonConvert.DeserializeObject<DataResult<int>>(content);
+            return _httpService.GetAsync<DataResult<int>>("/incomingvisitors/count");
         }
     }
 }
--- src/Asmin/Asmin.WebMVC/Services/Rest/UserService/UserApiService.cs
+++ src/Asmin/Asmin.WebMVC/Services/Rest/UserService/UserApiService.cs
@@ -1,79 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using Asmin.Core.Configuration.Context;
 using Asmin.Core.Entities.Concrete;
 using Asmin.Core.Utilities.Result;
 using Asmin.Entities.CustomEntities.Request.User;
 using Asmin.Entities.CustomEntities.Response.User;
-using Microsoft.AspNetCore.Http;
-using Newtonsoft.Json;
+using Asmin.WebMVC.Services.Rest.Base;
 
 namespace Asmin.WebMVC.Services.Rest.UserService
 {
     public class UserApiService : IUserApiService
     {
-        private readonly IHttpClientFactory _httpClientFactory;
-        private readonly IHttpContextAccessor _httpContextAccessor;
-        private readonly IAsminConfigurationContext _asminConfigurationContext;
+        private readonly IHttpService _httpService;
 
-        public UserApiService(IHttpClientFactory httpClientFactory, IAsminConfigurationContext asminConfigurationContext, IHttpContextAccessor httpContextAccessor)
+        public UserApiService(IHttpService httpService)
         {
-            _httpClientFactory = httpClientFactory;
-            _asminConfigurationContext = asminConfigurationContext;
-            _httpContextAccessor = httpContextAccessor;
+            _httpService = httpService;
         }
 
-        public async Task<DataResult<UserLoginResponse>> LoginAsync(UserLoginRequest loginRequest)
+        public Task<DataResult<UserLoginResponse>> LoginAsync(UserLoginRequest loginRequest)
         {
-            var httpClient = _httpClientFactory.CreateClient();
-
-            httpClient.BaseAddress = new Uri(_asminConfigurationContext.ApiUrl);
-
-            var httpContent = new StringContent(JsonConvert.SerializeObject(loginRequest), Encoding.UTF8, "application/json");
-
-            var responseMessage = await httpClient.PostAsync("/users/login", httpContent);
-
-            var content = await responseMessage.Content.ReadAsStringAsync();
-
-            return JsonConvert.DeserializeObject<DataResult<UserLoginResponse>>(content);
+            return _httpService.PostAsync<DataResult<UserLoginResponse>>("/users/login", loginRequest);
         }
 
-        public async Task<DataResult<List<User>>> GetListAsync()
+        public Task<DataResult<List<User>>> GetListAsync()
         {
-            var httpClient = _httpClientFactory.CreateClient();
-
-            _httpContextAccessor.HttpContext.Request.Cookies.TryGetValue("token", out string token);
-
-            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-
-            httpClient.BaseAddress = new Uri(_asminConfigurationContext.ApiUrl);
-
-            var responseMessage = await httpClient.GetAsync("/users");
-
-            var content = await responseMessage.Content.ReadAsStringAsync();
-
-            return JsonConvert.DeserializeObject<DataResult<List<User>>>(content);
+            return _httpService.GetAsync<DataResult<List<User>>>("/users");
         }
 
-        public async Task<DataResult<int>> GetCountAsync()
+        public Task<DataResult<int>> GetCountAsync()
         {
-            var httpClient = _httpClientFactory.CreateClient();
-
-            _httpContextAccessor.HttpContext.Request.Cookies.TryGetValue("token", out string token);
-
-            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-
-            httpClient.BaseAddress = new Uri(_asminConfigurationContext.ApiUrl);
-
-            var responseMessage = await httpClient.GetAsync("/users/count");
-
-            var content = await responseMessage.Content.ReadAsStringAsync();
-
-            return JsonConvert.DeserializeObject<DataResult<int>>(content);
+            return _httpService.GetAsync<DataResult<int>>("/users/count");
         }
     }
 }
