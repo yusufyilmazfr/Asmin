44a3aa96325a530a3d8a00c0d39cb790300dc916
Yusuf Yılmaz <yusufyilmazfr@gmail.com>
2021-01-30 23:12:43 +0300

    removed unnecessary namespaces.

--- src/Asmin/Asmin.WebAPI/Startup.cs
+++ src/Asmin/Asmin.WebAPI/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Asmin.Business.Extensions;
+﻿using Asmin.Business.Extensions;
 using Asmin.Core.Configuration.Context;
 using Asmin.Core.Configuration.Environment;
 using Asmin.Core.Extensions;
@@ -14,13 +10,10 @@ using Autofac;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Asmin.WebAPI
 {
